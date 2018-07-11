e824200746e0a47b210d7747f93cc68fd6f9b305
Selfless002 <40642559+dant02@users.noreply.github.com>
2018-07-12 00:27:02 +0200

    scripts and stylesheets setup for web

---
+++ web/app.ts
@@ -0,0 +1,8 @@
+﻿window.addEventListener('load', function (ev: Event) {
+    var txt = document.getElementById("txt");
+
+    if (txt)
+        txt.innerText = "Hello world " + new Date();
+    else
+        alert("element with id 'txt' not found");
+});
